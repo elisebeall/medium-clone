3e3f57eeb62d07f5d47c7e72ddff963cabe8503b
Ryan Pannone <Ryan_Pannone@mckinsey.com>
2022-12-09 13:19:04 -0800

    compoleted 12/9

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TagModule } from './tag/tag.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import ormconfig from '@app/ormconfig';
+
+@Module({
+  imports: [TypeOrmModule.forRoot(ormconfig), TagModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
