56ed21b8e96e1e6e25e908f98765be7bce5ca4b6
Elise Beall <elise_beall@mckinsey.com>
2022-12-16 11:36:09 -0700

    Initial commit

---
+++ src/ormconfig.ts
@@ -0,0 +1,19 @@
+// import { ConnectionOptions } from 'typeorm';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+const config: PostgresConnectionOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'mediumclone',
+  password: '1320',
+  database: 'mediumclone',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: false,
+  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
+};
+
+export default config;
---
+++ src/user/user.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from '@app/user/user.service';
+import { CreateUserDto } from '@app/user/dto/createUser.dto';
+import { UserEntity } from './user.entity';
+
+@Controller()
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('users')
+  async createUser(
+    @Body('user') createUserDto: CreateUserDto,
+  ): Promise<UserEntity> {
+    const user = await this.userService.createUser(createUserDto);
+    return this.userService.buildUserResponse(user);
+  }
+}
